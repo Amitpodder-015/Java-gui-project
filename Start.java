89063c95d3615fddefff5fe916d76a5e6f210ce8
Amit  Kumar Podder <amitpodder776@gmail.com>
2025-06-26 07:09:28 +0600

    Create Start.java

---
+++ Start.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Start {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            LoginFrame loginFrame = new LoginFrame();
+            loginFrame.setVisible(true);
+        });
+    }
+}
